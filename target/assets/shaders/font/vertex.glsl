cccc9d9b77c8bebf67be9dc45cffc69ccb5551d1
kuviman <kuviman@gmail.com>
2026-01-24 13:03:54 +0400

    fix font texture bleeding

--- target/assets/shaders/font/vertex.glsl
+++ target/assets/shaders/font/vertex.glsl
@@ -11,8 +11,10 @@ uniform vec2 u_uv_rect_size;
 uniform vec2 u_pos;
 uniform vec2 u_size;
 
+#define FIX_BLEEDING 0.99
+
 void main() {
-    vec2 local_uv = a_pos * 0.5 + 0.5;
+    vec2 local_uv = a_pos * FIX_BLEEDING * 0.5 + 0.5;
     v_uv = u_uv_rect_pos + local_uv * u_uv_rect_size;
     vec2 pos = local_uv * u_size + u_pos;
     vec3 screen_pos = u_projection_matrix * u_view_matrix * vec3(pos, 1.0);
